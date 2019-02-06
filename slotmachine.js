503000a0406a93b144493d4a1ef899da717446a1
Brasseur Thibault <brasseur.blz@gmail.com>
2019-02-06 16:16:12 +0100

    commit js nbaléatoire

--- slotmachine.js
+++ slotmachine.js
@@ -1,1 +1,11 @@
 
+var tab = [ "restaurant" , "bar" , "maison" ] ;
+var length = tab.length-1;
+
+function nbaleatoire(x){
+  var a = Math.random()*x;
+  var b = Math.round(a);
+  return b;
+}
+var resultlieux = nbaleatoire(length);
+console.log (tab[resultlieux]);
