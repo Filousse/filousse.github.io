62085859c534861168d975b0b12596c0bc180351
Brasseur Thibault <brasseur.blz@gmail.com>
2019-02-07 14:07:19 +0100

    input resultat

--- slotmachine.js
+++ slotmachine.js
@@ -18,16 +18,16 @@ var liste1 = [ "restaurant" , "bar" , "maison" ] ;
 var liste2 = [ "chaussures" , "casquette" , "pull"];
 var liste3 = [ "foto" , "video" , "audio" ] ;
 
-
 $(function(){
-
-  $("#start").click(function(){
-    var result1 = pioche(liste1);
-    var result2 = pioche(liste2);
-    var result3 = pioche(liste3);
-    console.log(result1);
-    console.log(result2);
-    console.log(result3);
-
-  });
+  var elmt = $('#start');
+  elmt.hover(function() {
+    $(this).css({
+      'color' : 'red'
+    });
+  })
+  var elmtrg = $('#resultatglobal')
+  var elmt = $('#start');
+  elmt.click(function() {
+    $('#resultatglobal').val('2');
+    });
 });
