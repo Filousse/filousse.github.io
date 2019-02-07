401b07970423f4861323d4f0135bb878bac763cc
Brasseur Thibault <brasseur.blz@gmail.com>
2019-02-07 12:00:20 +0100

    EVENT MULTIPIOCHE

--- slotmachine.js
+++ slotmachine.js
@@ -1,11 +1,33 @@
-
-var tab = [ "restaurant" , "bar" , "maison" ] ;
-var length = tab.length-1;
-
 function nbaleatoire(x){
   var a = Math.random()*x;
   var b = Math.round(a);
   return b;
 }
-var resultlieux = nbaleatoire(length);
-console.log (tab[resultlieux]);
+
+
+
+function pioche(l) {
+  var length = l.length-1;
+  var result = nbaleatoire(length);
+  return l[result];
+}
+
+
+
+var liste1 = [ "restaurant" , "bar" , "maison" ] ;
+var liste2 = [ "chaussures" , "casquette" , "pull"];
+var liste3 = [ "foto" , "video" , "audio" ] ;
+
+
+$(function(){
+
+  $("#start").click(function(){
+    var result1 = pioche(liste1);
+    var result2 = pioche(liste2);
+    var result3 = pioche(liste3);
+    console.log(result1);
+    console.log(result2);
+    console.log(result3);
+
+  });
+});
