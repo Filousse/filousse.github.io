de5207d3f45d8d6594a8896bb4f8e0c5699c491a
Brasseur Thibault <brasseur.blz@gmail.com>
2019-02-12 14:54:47 +0100

    FOR / ULs + animate

--- slotmachineefect.js
+++ slotmachineefect.js
@@ -1,10 +1,22 @@
 $(function(){
     var elmt = $('#start');
     elmt.click(function() {
-      var off = nbaleatoire(4) * 100;
-      var margin = '-' + off + 'px';
-      $('ul').css({
-        'margin-top': margin
-      });
+
+      var l1 = $('#ul1');
+      var l2 = $('#ul2');
+      var l3 = $('#ul3');
+
+      var uls = [l1, l2, l3];
+
+      for(var i=0; i<uls.length; i++) {
+        var ul = uls[i];
+        var off = nbaleatoire(4) * 100;
+        var margin = '-' + off + 'px';
+
+        ul.animate({
+          'margin-top': margin
+
+        });
+      }
     });
 });
