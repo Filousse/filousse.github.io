8570d422376b1b826be6fb29b92ac8279ee040dc
Brasseur Thibault <brasseur.blz@gmail.com>
2019-02-15 12:27:42 +0100

    JS cleaner + décalge de tps

--- slotmachine.js
+++ slotmachine.js
@@ -10,32 +10,25 @@ function pioche(l) {
   return l[result];
 }
 
-var liste1 = [ "restaurant" , "bar" , "maison" ] ;
-var liste2 = [ "chaussures" , "casquette" , "pull"];
-var liste3 = [ "foto" , "video" , "audio" ] ;
+var liste1 = [ "restaurant" , "bar" , "maison", "a" , "b" ,"c" , "d", "k" , "f" , "g" ] ;
+var liste2 = [ "chaussures" , "casquette" , "pull" , "a" , "b" ,"c" , "d", "e" , "f" , "g"];
+var liste3 = [ "foto" , "video" , "audio" , "a" , "b" ,"c" , "d", "e" , "f" , "g" ] ;
+var listes = [ liste1, liste2, liste3 ] ;
 
-$(function(){
-  var elmt = $('#start');
-  elmt.hover(function() {
-    $(this).css({
-      'color' : 'red'
-    });
-})
 
-  var nb1 = pioche(liste1);
-  var nb2 = pioche(liste2);
-  var nb3 = pioche(liste3);
-  var elmtrg = $('#resultatglobal');
-  var elmt = $('#start');
-  elmt.click(function() {
-    $(elmtrg).val('Vas dans un(e) ' + nb1 + ' habillé d un(e) ' + nb2 + ' et fais un(e) ' + nb3);
-    });
-});
 
 $(function(){
     var elmt = $('#start');
+    var elmtrg = $('#resultatglobal');
+
+    elmt.hover(function() {
+        $(this).css({
+            'color' : 'red'
+        });
+    });
+
     elmt.click(function() {
-      var off = nbaleatoire(4) * 100;
+      var off = nbaleatoire(9) * 100;
       var margin = '-' + off + 'px';
       $('ul').css({
         'margin-top': margin
@@ -44,18 +37,30 @@ $(function(){
       var l1 = $('#ul1');
       var l2 = $('#ul2');
       var l3 = $('#ul3');
-
       var uls = [l1, l2, l3];
+      var mots = [];
 
-      for(var i=0; i<uls.length; i++) {
+      for(var i = 0; i < uls.length; i++) {
         var ul = uls[i];
-        var off = nbaleatoire(9) * 100;
+        var index = nbaleatoire(9);
+        var liste = listes[i];
+        var mot = liste[index];
+        mots.push(mot);
+        var off = index * 100;
         var margin = '-' + off + 'px';
-
-        ul.animate({
+        var props = {
           'margin-top': margin
+        };
+        var vitesse = 1000;
 
-        });
+        if (i < 2) {
+          ul.animate(props, vitesse);
+        } else {
+          ul.animate(props, vitesse , function() {
+            $(elmtrg).val('Vas dans un(e) ' + mots[0] + ' habillé d un(e) ' + mots[1] + ' et fais un(e) ' + mots[2]);
+          });
+        }
+
       }
     });
 });
